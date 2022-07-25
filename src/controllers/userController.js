3571e33d8cfec66777a1a86e349b926fa36418c0
adiyadav0 <adiyadav596@gmail.com>
2022-07-25 19:04:11 +0530

    getapi

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -201,8 +201,24 @@ const userLogin = async function (req, res) {
 }
 
 
+///////////////////////////
+const getUser = async function(req,res){
+    try{
+    let userId = req.params.userId
+    if(!mongoose.isValidObjectId(userId)){
+        return res.status(400).send({status:false,message:"userId is invalid"});
+      }  
+    let userData= await userModel.findById(userId)
+    if(!userData){
+        return res.status(404).send({status:false, message:"No user found"})
+    }
+    return res.status(200).send({status:true,message:"User profile details",data:userData})
 
-
+}catch(error){
+    res.status(500).send({status:false,message: error.message})
+}
+}
+////////////////
 
 
 
@@ -349,4 +365,4 @@ const updateData = async function (req, res) {
 
 
 
-module.exports = { createUser, userLogin, updateData }
+module.exports = { createUser, userLogin, updateData,getUser }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {createUser, userLogin, updateData} = require('../controllers/userController')
+const {createUser, userLogin,getUser, updateData} = require('../controllers/userController')
 
 router.post('/register', createUser)
 router.post('/login', userLogin)
 router.put('/user/:userId/profile', updateData)
+
+router.get('/user/:userId/profile', getUser)
+
+router.all('/**', function(req,res){
+    res.status(400).send({status:false, message:"Invalid params"})
+})
 
 module.exports = router;
